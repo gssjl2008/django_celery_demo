71af2f1f2b15e615be4cc0b39e76ff41df176da5
wx <gssjl2004@126.com>
2020-04-18 21:54:20 +0800

    test

--- sort_demo/tests.py
+++ sort_demo/tests.py
@@ -16,9 +16,13 @@ def show(func):
     def _inner(data):
         print(f"data: {data}")
         tmp = data
+        print(f"tmp:{tmp}")
         result = func(data)
         print(f"result: {result}")
-        print("result", sorted(tmp) == result)
+        if sorted(tmp) == result:
+            print("result: True")
+        else:
+            print(f"sorted: {sorted(tmp)}")
         return result
     return _inner
 
@@ -54,8 +58,44 @@ def insert_sort(data):
 
     return data
 
+@show
+def select_sort(data):
+    length = len(data)
+    if length <= 1:
+        return data
+
+    for i in range(length - 1):
+        index = i
+        for j in range(i + 1, length):
+            if data[index] > data[j]:
+                index = j
+        data[i], data[index] = data[index], data[i]
+    return data
+
+@show
+def quick_sort(data):
+
+    def _quick_sort(data):
+        if len(data) <= 1:
+            return data
+
+        mid = data[len(data) // 2]
+        print(f"mid: {mid}")
+        left, right = [], []
+        data.remove(mid)
+
+        for i in data:
+            if i < mid: left.append(i)
+            else: right.append(i)
+        
+        return _quick_sort(left) + [mid] + _quick_sort(right)
+
+    result = _quick_sort(data)
+    
+    return result
+
 
 if __name__ == '__main__':
     data = get_data()
     # data = [1,5,4,3,2]
-    insert_sort(data)
+    quick_sort(data)
